d7d455c973cf6342b423ec8d071814b5dbd3e8f7
Miguel Zapata <miguezaga2@hotmail.com>
2025-03-18 13:08:32 +1100

    added fonts and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,50 @@
 import "./App.css";
 
+const data = {
+  quizzes: [
+    {
+      title: "HTML",
+      icon: "./assets/images/icon-html.svg",
+      questions: [
+        {
+          question: "What does HTML stand for?",
+          options: [
+            "Hyper Trainer Marking Language",
+            "Hyper Text Marketing Language",
+            "Hyper Text Markup Language",
+            "Hyper Text Markup Leveler"
+          ],
+          answer: "Hyper Text Markup Language"
+        }
+      ]
+    }
+  ]
+};
+
 function App() {
   return (
     <>
-      <h1 className="mt-10 text-center text-4xl">
-        Welcome to the Frontend Quiz!
-      </h1>
+      <div className="min-h-screen bg-gray-100 font-thin">
+        <header className="h-20">
+          <div>{/* Question title */}</div>
+          <div>{/* Light/dark toggle */}</div>
+        </header>
+        <main className="text-blue-900">
+          <div className="mx-auto max-w-xl">
+            <h1 className="text-center text-4xl">
+              Welcome to the <span className="font-medium">Frontend Quiz!</span>
+            </h1>
+            <p className="italic">Pick a subject to get started.</p>
 
-      {/*
-      <!-- Quiz menu start -->
-
-      Welcome to the Frontend Quiz!
-      Pick a subject to get started.
-
-      HTML
-      CSS
-      JavaScript
-      Accessibility
-
-      <!-- Quiz menu end -->
-
-      <!-- Quiz question start -->
-
-      Question <!-- number --> of 10
-
-      A
-      B
-      C
-      D
-
-      Submit answer
-
-      <!-- Quiz question end -->
-
-      <!-- Quiz completed start -->
-
-      Quiz completed
-      You scored...
-
-      <!-- score --> out of 10
-
-      <!-- Quiz completed end -->
-
-      */}
+            <ul className="font-medium">
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>Accessibility</li>
+            </ul>
+          </div>
+        </main>
+      </div>
     </>
   );
 }
