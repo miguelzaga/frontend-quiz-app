798650318fbe219bd2da0da919ee033cdd7dec67
Miguel Zapata <miguezaga2@hotmail.com>
2025-03-19 16:37:12 +1100

    added list li, styled layout and some colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,69 @@
 import "./App.css";
+import iconMoonDark from "./assets/images/icon-moon-dark.svg";
+import iconSunDark from "./assets/images/icon-sun-dark.svg";
+import iconHtml from "./assets/images/icon-html.svg";
+import iconCss from "./assets/images/icon-css.svg";
+import iconJs from "./assets/images/icon-js.svg";
+import iconAccessibility from "./assets/images/icon-accessibility.svg";
 
-const data = {
-  quizzes: [
-    {
-      title: "HTML",
-      icon: "./assets/images/icon-html.svg",
-      questions: [
-        {
-          question: "What does HTML stand for?",
-          options: [
-            "Hyper Trainer Marking Language",
-            "Hyper Text Marketing Language",
-            "Hyper Text Markup Language",
-            "Hyper Text Markup Leveler"
-          ],
-          answer: "Hyper Text Markup Language"
-        }
-      ]
-    }
-  ]
+/*
+TODO: I need to change the liTopics for the data itself. I think is better if
+I do that in another file. And depending on the data needed, I can transform
+it and then render it in this component
+*/
+const liTopics = {
+  HTML: iconHtml,
+  CSS: iconCss,
+  JavaScript: iconJs,
+  Accessibility: iconAccessibility
 };
 
 function App() {
   return (
     <>
       <div className="min-h-screen bg-gray-100 font-thin">
-        <header className="h-20">
-          <div>{/* Question title */}</div>
-          <div>{/* Light/dark toggle */}</div>
-        </header>
-        <main className="text-blue-900">
-          <div className="mx-auto max-w-xl">
-            <h1 className="text-center text-4xl">
-              Welcome to the <span className="font-medium">Frontend Quiz!</span>
-            </h1>
-            <p className="italic">Pick a subject to get started.</p>
+        <div className="mx-auto max-w-29">
+          <header className="px-6 py-4">
+            <div>{/* Question title */}</div>
+            <div className="ml-auto flex w-fit">
+              <img src={iconMoonDark} alt="" />
+              <input type="checkbox" />
+              <img src={iconSunDark} alt="" />
+            </div>
+          </header>
+          <main className="grid gap-y-10 px-6 py-8 text-blue-900">
+            <div>
+              <h1 className="text-4xl">
+                Welcome to the{" "}
+                <span className="font-medium">Frontend Quiz!</span>
+              </h1>
+              <p className="mt-4 italic">Pick a subject to get started.</p>
+            </div>
 
-            <ul className="font-medium">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>Accessibility</li>
+            <ul className="grid gap-y-3 font-medium">
+              {Object.entries(liTopics).map(liElement)}
             </ul>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
     </>
+  );
+}
+
+function liElement(keyPair, index) {
+  const [text, img] = keyPair;
+  return (
+    <li
+      className="flex items-center gap-x-4 rounded-xl bg-white p-3"
+      key={`liElement-${index}`}
+    >
+      <div>
+        {" "}
+        {/* TODO: add background that changes according to the input */}
+        <img src={img} alt="" />
+      </div>
+      <p>{text}</p>
+    </li>
   );
 }
 
